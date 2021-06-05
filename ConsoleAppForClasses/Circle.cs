439f7380372d955c9cc34868cd2b6f37b1c1d67d
farazz91 <farazz91@gmail.com>
2021-06-05 15:00:05 +0530

    Static and instance class members

---
+++ ConsoleAppForClasses/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppForClasses
+{
+    class Circle
+    {
+        static float pi;
+        float radius;
+
+        public Circle(int radius)
+        {
+            pi = 3.142F;
+            this.radius = radius;
+        }
+
+        public float calculateRadius()
+        {
+            return (pi * radius * radius);
+        }
+    }
+}
--- ConsoleAppForClasses/Program.cs
+++ ConsoleAppForClasses/Program.cs
@@ -11,6 +11,12 @@ namespace ConsoleAppForClasses
 
             Customer customer2 = new Customer();
             customer2.PrintFullName();
+
+            Circle circle1 = new Circle(5);
+            Console.WriteLine("Radius of circle is: {0}" ,circle1.calculateRadius().ToString());
+
+            Circle circle2 = new Circle(9);
+            Console.WriteLine("Radius of circle is: {0}", circle2.calculateRadius().ToString());
         }
     }
 }
